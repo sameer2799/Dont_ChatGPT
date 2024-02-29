35b7c59b246242076e976784773e852214dbd034
Sameer Singh <sameersingh2799@gmail.com>
2024-02-29 21:33:27 +0530

    Initial Setup

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+const port = 5000;
+
+app.get("/hello", (req, res, next) => {
+  return res.send('Hello World');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
