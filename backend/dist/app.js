b6c814f6233fc90507fafa66e106010cfa2f1779
Sameer Singh <90849260+sameer2799@users.noreply.github.com>
2024-03-11 16:45:37 +0000

    newer

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { config } from 'dotenv';
+import morgan from "morgan";
 config();
 const app = express();
+// remove in production
+app.use(morgan('dev'));
 // middlewares
 app.use(express.json());
+app.use('/api/v1');
 export default app;
 //# sourceMappingURL=app.js.map
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import { config } from 'dotenv';
+import morgan from "morgan";
 
 config();
 const app = express();
 
+// remove in production
+app.use(morgan('dev'));
+
+
 // middlewares
 app.use(express.json());
+
+app.use('/api/v1');
 
 export default app;
